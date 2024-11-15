250c28804dc736a73c3c51fa07e24f1a38a5c6e8
Leonardo Bertonasco <leonardo.bertonasco01@gmail.com>
2024-11-15 23:46:08 +0100

    changed to use double instance

--- myClasses/Utilities/Math/FT_Math_Imperial.cs
+++ myClasses/Utilities/Math/FT_Math_Imperial.cs
@@ -2,17 +2,14 @@ namespace MyClasses
 {
   public partial class FT_Math
   {
-    public static class Imperial
-    {
-      public static double InchToCentimeter(double inch) => inch / 2.54;
-      public static double FootToCentimeter(double foot) => foot / 30.48;
-      public static double YardToCentimeter(double yard) => yard / 91.44;
-      public static double MileToKilometer(double mile) => mile / 1.609;
-      public static double OunceToGramm(double ounce) => ounce;
-      public static double PoundToGramm(double pound) => pound;
-      public static double StoneToKilogramm(double stone) => stone;
-      public static double PintToMilliliter(double pin) => pin;
-      public static double GallonToLiter(double gallon) => gallon;
-    }
+      public static double InchToCentimeter(this double inch) => inch / 2.54;
+      public static double FootToCentimeter(this double foot) => foot / 30.48;
+      public static double YardToCentimeter(this double yard) => yard / 91.44;
+      public static double MileToKilometer(this double mile) => mile / 1.609;
+      public static double OunceToGramm(this double ounce) => ounce;
+      public static double PoundToGramm(this double pound) => pound;
+      public static double StoneToKilogramm(this double stone) => stone;
+      public static double PintToMilliliter(this double pin) => pin;
+      public static double GallonToLiter(this double gallon) => gallon;
   }
 }
