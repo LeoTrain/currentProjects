7182d0accb9c57477628b2252a8d5e521f6bfdd9
FTL_Ubuntu <leonardo.bertonasco01@gmail.com>
2024-12-03 00:26:51 +0100

    learned a bit of terminal gui

--- TcpProject/Client/ClienUI.cs
+++ TcpProject/Client/ClienUI.cs
@@ -7,12 +7,33 @@ namespace Client
     {
         private const ConsoleColor _backgroundColor = ConsoleColor.White;
         private const ConsoleColor _foregroundColor = ConsoleColor.Black;
+        string currenttxt = "";
+
+        private Window InitLoginWindow()
+        {
+            Window loginWindow = new Window("Login")
+            {
+                X = 0,
+                Y = 1,
+                Width = Dim.Fill(),
+                Height = Dim.Fill(),
+                ColorScheme = new ColorScheme
+                {
+                    Normal = Application.Driver.MakeAttribute(Color.White, Color.DarkBlue), // Texte et fond normaux
+                    Focus = Application.Driver.MakeAttribute(Color.Black, Color.Gray),     // Texte et fond en focus
+                    HotNormal = Application.Driver.MakeAttribute(Color.Cyan, Color.DarkBlue), // Texte souligné (HotKey)
+                    HotFocus = Application.Driver.MakeAttribute(Color.Black, Color.Cyan)      // Texte souligné en focus
+                }
+            };
+            return loginWindow;
+
+        }
 
         public void Start()
         {
             Terminal.Gui.Toplevel top = Application.Top;
 
-            Window window = new Window("Chit Chat")
+            Window secondWindow = new Window("Second window")
             {
                 X = 0,
                 Y = 1,
@@ -24,7 +45,8 @@ namespace Client
                     {
                         new MenuBarItem("_File", new MenuItem[]
                         {
-                            new MenuItem("_Quit", "Quit the application", () => Application.RequestStop())
+                            new MenuItem("_Quit", "Quit the application", () => Application.RequestStop()),
+                            new MenuItem("_New Window", "Open a new window", () => top.Add(secondWindow)),
                         }),
                         new MenuBarItem("_Help", new MenuItem[]
                         {
@@ -54,9 +76,77 @@ namespace Client
             /*button2.Clicked += () => Application.RequestStop();*/
             /*window.Add(label, button, button2);*/
 
+            TextField textField = new TextField("")
+            {
+                X = Pos.Center() - 20,
+                Y = Pos.Center() - 2,
+                Width = 20,
+            };
+
+            TextField textFieldPsd = new TextField("")
+            {
+                X = Pos.Center() - 20,
+                Y = Pos.Center() + 2,
+                Width = 20,
+                Secret = true,
+            };
+
+            Button submitButton = new Button("Submit")
+            {
+                X = Pos.Center() + 10,
+                Y = Pos.Center(),
+            };
+
+            submitButton.Clicked += () =>
+            {
+                Label label = new Label("This is a dialog!")
+                {
+                    X = Pos.Center(),
+                    Y = Pos.Center()
+                };
+                TextField txt = new TextField("")
+                {
+                    X = Pos.Center(),
+                    Y = Pos.Center() + 5,
+                    Width = 20,
+                };
+                Button btn2 = new Button("Close");
+                Button btn3 = new Button("Check");
+                btn2.Clicked += OnBtnClick;
+                btn3.Clicked += () => MessageBox.Query("Text", txt.Text.ToString(), "OK");
+                Dialog dialog = new Dialog("My Dialog", 50, 20, btn2, btn3);
+
+
+                dialog.Add(label, txt);
+                Application.Run(dialog);
+            };
+            window.Add(textField,textFieldPsd, submitButton);
+
             top.Add(menu, window);
             Application.Run();
         }
+
+        private void OnBtnClick()
+        {
+            Application.RequestStop();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         private void Display()
         {
