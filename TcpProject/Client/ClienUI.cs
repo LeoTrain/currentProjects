8fffea9df1aacbf70c79bca1c7caffdc6a8509ea
Leonardo Bertonasco <leonardo.bertonasco01@gmail.com>
2024-12-03 02:55:50 +0100

    basic looks and chat

--- TcpProject/Client/ClienUI.cs
+++ TcpProject/Client/ClienUI.cs
@@ -1,5 +1,4 @@
 using Terminal.Gui;
-using System.Threading;
 
 namespace Client
 {
@@ -8,127 +7,126 @@ namespace Client
         private const ConsoleColor _backgroundColor = ConsoleColor.White;
         private const ConsoleColor _foregroundColor = ConsoleColor.Black;
         string currenttxt = "";
-
-        private Window InitLoginWindow()
+        private ColorScheme _mainColorScheme = new ColorScheme
         {
-            Window loginWindow = new Window("Login")
+            Normal = Application.Driver.MakeAttribute(Color.White, Color.Blue),
+            Focus = Application.Driver.MakeAttribute(Color.Black, Color.White),
+            HotNormal = Application.Driver.MakeAttribute(Color.Cyan, Color.Blue),
+            HotFocus = Application.Driver.MakeAttribute(Color.White, Color.Cyan)
+        };
+        private ColorScheme _drakulaColorScheme = new ColorScheme
+        {
+            Normal = Application.Driver.MakeAttribute(Color.BrightMagenta, Color.Black),
+            Focus = Application.Driver.MakeAttribute(Color.White, Color.BrightMagenta),
+            HotNormal = Application.Driver.MakeAttribute(Color.BrightCyan, Color.Black),
+            HotFocus = Application.Driver.MakeAttribute(Color.BrightYellow, Color.BrightMagenta)
+        };
+        private ColorScheme _chatColorScheme = new ColorScheme
+        {
+            Normal = Application.Driver.MakeAttribute(Color.White, Color.Black),
+            Focus = Application.Driver.MakeAttribute(Color.Black, Color.Gray)
+        };
+        private ColorScheme _gruvboxDarkColorScheme = new ColorScheme {
+            Normal = Application.Driver.MakeAttribute(Color.BrightYellow, Color.Black),
+            Focus = Application.Driver.MakeAttribute(Color.White, Color.DarkGray),
+            HotNormal = Application.Driver.MakeAttribute(Color.BrightRed, Color.Black),
+            HotFocus = Application.Driver.MakeAttribute(Color.BrightYellow, Color.DarkGray)
+        };
+        private ColorScheme _solarizedLightColorScheme = new ColorScheme {
+            Normal = Application.Driver.MakeAttribute(Color.Black, Color.BrightYellow),
+            Focus = Application.Driver.MakeAttribute(Color.Blue, Color.White),
+            HotNormal = Application.Driver.MakeAttribute(Color.BrightGreen, Color.BrightYellow),
+            HotFocus = Application.Driver.MakeAttribute(Color.Red, Color.White)
+        };
+
+
+        private Window InitChatWindow()
+        {
+            Window chatWindow = new Window("Chat")
             {
                 X = 0,
                 Y = 1,
                 Width = Dim.Fill(),
                 Height = Dim.Fill(),
-                ColorScheme = new ColorScheme
+                ColorScheme = _gruvboxDarkColorScheme,
+            };
+
+            TextView messageDisplay = new TextView
+            {
+                X = 0,
+                Y = 1,
+                Width = Dim.Fill(),
+                Height = Dim.Fill() - 3,
+                ReadOnly = true,
+                Text = $"You joined the chat at {DateTime.Now:HH:mm}\n"
+            };
+
+            TextField inputField = new TextField("==> ")
+            {
+                X = 1,
+                Y = Pos.Bottom(messageDisplay) + 1,
+                Width = Dim.Fill() - 12
+            };
+
+            Button sendMessageButton = new Button("Send")
+            {
+                X = Pos.Right(inputField) + 1,
+                Y = Pos.Bottom(messageDisplay) + 1
+            };
+
+            sendMessageButton.Clicked += () =>
+            {
+                string message = inputField.Text.ToString() ?? "ErrorSendMessageButton";;
+                if (!string.IsNullOrWhiteSpace(message))
                 {
-                    Normal = Application.Driver.MakeAttribute(Color.White, Color.DarkBlue), // Texte et fond normaux
-                    Focus = Application.Driver.MakeAttribute(Color.Black, Color.Gray),     // Texte et fond en focus
-                    HotNormal = Application.Driver.MakeAttribute(Color.Cyan, Color.DarkBlue), // Texte souligné (HotKey)
-                    HotFocus = Application.Driver.MakeAttribute(Color.Black, Color.Cyan)      // Texte souligné en focus
+                    Application.MainLoop.Invoke(() =>
+                    {
+                        string currentMessages = messageDisplay.Text.ToString() ?? "";
+                        messageDisplay.Text = currentMessages + $"{DateTime.Now:HH:mm}: {message}\n";
+                    });
+                    inputField.Text = "==> ";
                 }
             };
-            return loginWindow;
 
-        }
-
-        public void Start()
-        {
-            Terminal.Gui.Toplevel top = Application.Top;
-
-            Window secondWindow = new Window("Second window")
+            inputField.KeyPress += (args) =>
             {
-                X = 0,
-                Y = 1,
-                Width = Dim.Fill(),
-                Height = Dim.Fill(),
+                if (args.KeyEvent.Key == Key.Enter)
+                {
+                    sendMessageButton.OnClicked();
+                    args.Handled = true;
+                }
             };
 
+            chatWindow.Add(inputField, messageDisplay, sendMessageButton);
+            chatWindow.FocusFirst();
+
+            return chatWindow;
+        }
+
+        private MenuBar InitMenuBar()
+        {
             MenuBar menu = new MenuBar(new MenuBarItem[]
                     {
                         new MenuBarItem("_File", new MenuItem[]
                         {
-                            new MenuItem("_Quit", "Quit the application", () => Application.RequestStop()),
-                            new MenuItem("_New Window", "Open a new window", () => top.Add(secondWindow)),
+                            new MenuItem("_Quit", "Quit the application", () => Application.RequestStop())
                         }),
                         new MenuBarItem("_Help", new MenuItem[]
                         {
                             new MenuItem("_About", "About this app", () => MessageBox.Query("About", "Blablablablabla", "ok"))
                         }),
                     });
-                
-
-            /*Label label = new Label("Hello ...")*/
-            /*{*/
-            /*    X = Pos.Center(),*/
-            /*    Y = Pos.Center() - 5,*/
-            /*};*/
-            /**/
-            /*Button button = new Button("Click me")*/
-            /*{*/
-            /*    X = Pos.Center(),*/
-            /*    Y = Pos.Center() - 1,*/
-            /*};*/
-            /**/
-            /*Button button2 = new Button("Quit")*/
-            /*{*/
-            /*    X = Pos.Center() - 20,*/
-            /*    Y = Pos.Center() - 1,*/
-            /*};*/
-            /*button.Clicked += () => MessageBox.Query("Button clicked", "This is the message that you clicked", "Ok");*/
-            /*button2.Clicked += () => Application.RequestStop();*/
-            /*window.Add(label, button, button2);*/
-
-            TextField textField = new TextField("")
-            {
-                X = Pos.Center() - 20,
-                Y = Pos.Center() - 2,
-                Width = 20,
-            };
-
-            TextField textFieldPsd = new TextField("")
-            {
-                X = Pos.Center() - 20,
-                Y = Pos.Center() + 2,
-                Width = 20,
-                Secret = true,
-            };
-
-            Button submitButton = new Button("Submit")
-            {
-                X = Pos.Center() + 10,
-                Y = Pos.Center(),
-            };
-
-            submitButton.Clicked += () =>
-            {
-                Label label = new Label("This is a dialog!")
-                {
-                    X = Pos.Center(),
-                    Y = Pos.Center()
-                };
-                TextField txt = new TextField("")
-                {
-                    X = Pos.Center(),
-                    Y = Pos.Center() + 5,
-                    Width = 20,
-                };
-                Button btn2 = new Button("Close");
-                Button btn3 = new Button("Check");
-                btn2.Clicked += OnBtnClick;
-                btn3.Clicked += () => MessageBox.Query("Text", txt.Text.ToString(), "OK");
-                Dialog dialog = new Dialog("My Dialog", 50, 20, btn2, btn3);
-
-
-                dialog.Add(label, txt);
-                Application.Run(dialog);
-            };
-            window.Add(textField,textFieldPsd, submitButton);
-
-            top.Add(menu, window);
-            Application.Run();
+            return menu;
         }
 
-        private void OnBtnClick()
+        public void Start()
         {
-            Application.RequestStop();
+            Terminal.Gui.Toplevel top = Application.Top;
+            Window chatWindow = InitChatWindow();
+            MenuBar menuBar = InitMenuBar();
+
+            top.Add(menuBar, chatWindow);
+            Application.Run();
         }
 
 
