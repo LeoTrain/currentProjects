8b1d01593eecfce077785d39597482e37fd63da8
Leonardo Bertonasco <leonardo.bertonasco01@gmail.com>
2024-12-06 03:15:12 +0100

    no problems with lsp

--- MarioIsaac/entities/base_character.py
+++ MarioIsaac/entities/base_character.py
@@ -8,6 +8,10 @@ from ..logic.event_dick import event_dick
 class BaseCharacter(MovingTile):
     def __init__(self, display, sprite_sheet_path):
         super().__init__(display)
+        self.number_of_frames = {}
+        self.sprite_widths = {}
+        self.sprite_heights = {}
+
         self.sprite_loader = SpriteLoader(sprite_sheet_path)
         self.animation_controller = AnimationController(self.sprite_loader, self)
         self.sprites = self.sprite_loader.load_character_sprites(self.number_of_frames, self.sprite_widths, self.sprite_heights)
@@ -25,14 +29,12 @@ class BaseCharacter(MovingTile):
 
 
     def take_damage(self, damage):
-        from ..entities.player import Player
-        from ..entities.goblin import Goblin
         self.life_points -= damage
         if self.life_points <= 0:
-            if isinstance(self, Player):
+            if self.isPlayer():
                 event = pygame.event.Event(event_dick["player_dead"])
                 pygame.event.post(event)
-            elif isinstance(self, Goblin):
+            elif self.isEnemy:
                 event = pygame.event.Event(event_dick["enemy_dead"])
                 pygame.event.post(event)
 
